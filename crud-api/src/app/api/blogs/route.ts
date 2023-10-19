a328301b90c8377b85f673c5ba4646e412d93c93
KurehaRyosuke <ohhayo.com@gmail.com>
2023-10-19 16:14:57 +0900

    test: postman of api test

--- crud-api/src/app/api/blogs/route.ts
+++ crud-api/src/app/api/blogs/route.ts
@@ -1,7 +1,45 @@
+import { addPosts, getPosts } from "@/lib/data";
+import { NextResponse } from "next/server";
+
 export const GET = async (req: Request, res: Response) => {
   console.log("GET REQUEST");
+  try {
+    const posts = getPosts();
+
+    return NextResponse.json({ message: "OK", posts }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }.error, {
+      status: 500,
+    });
+  }
 };
 
 export const POST = async (req: Request, res: Response) => {
   console.log("POST REQUEST");
+  try {
+    const { title, description } = await req.json();
+
+    const post = {
+      id: Date.now().toString(),
+      title,
+      description,
+      date: new Date(),
+    };
+
+    addPosts(post);
+
+    return NextResponse.json({ message: "OK", post }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }.error, {
+      status: 500,
+    });
+  }
 };
+
+/**
+ * postman
+ * {
+ *  "title": "My new POST12",
+ *  "description": "description sample"
+ * }
+ */
