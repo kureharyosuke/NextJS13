190d7fb4023f790705b6cdf37f4f2d0a7c34d3f1
KurehaRyosuke <ohhayo.com@gmail.com>
2023-12-21 13:06:27 +0900

    add: sign-in and sign-up of page added

--- next14-event/components/shared/Header.tsx
+++ next14-event/components/shared/Header.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+// import NavItems from "./NavItems"
+// import MobileNav from "./MobileNav"
 
 export const Header = () => {
   return (
@@ -16,6 +20,18 @@ export const Header = () => {
             alt="LOGO"
           />
         </Link>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            {/* <MobileNav /> */}
+          </SignedIn>
+
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
         <div>Login</div>
       </nav>
     </header>
