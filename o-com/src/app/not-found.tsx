f1edacea3120977ccc806c4e1a28918d8fe2c348
KurehaRyosuke <ohhayo.com@gmail.com>
2024-01-03 23:00:11 +0900

    add: not-found and page added

---
+++ o-com/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import { NextPage } from "next";
+
+export const NotFound: NextPage = () => {
+  return (
+    <div>
+      <p>Sorry, Not Found Page</p>
+      <Link href={"/search"}>Search</Link>
+    </div>
+  );
+};
