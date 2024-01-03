2fa9310a8c3fd310a81873dd090c2f740eb53337
KurehaRyosuke <ohhayo.com@gmail.com>
2024-01-03 23:18:16 +0900

    update: page add and layout added

--- o-com/src/app/compose/tweet/page.tsx
+++ o-com/src/app/compose/tweet/page.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import stylex from "@stylexjs/stylex";
 
-export const page = () => {
-  return <div>page</div>;
+const styles = stylex.create({
+  main: {
+    width: "100%",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    // 태블릿
+    "@media (max-width: 768px)": {
+      width: "100%",
+    },
+    // 모바일
+    "@media (max-width: 480px)": {
+      width: "100%",
+    },
+  },
+});
+export const Home = () => {
+  return <main className={stylex(styles.main)}></main>;
 };
